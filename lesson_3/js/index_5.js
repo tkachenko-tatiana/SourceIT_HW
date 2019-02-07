de7a21150fdc454047a491b82aa4dfcf92cb2010
Aleksandr <41649913+MachalovAlexandr@users.noreply.github.com>
2019-02-07 19:43:58 +0200

    Update index_5.js

--- lesson_3/js/index_5.js
+++ lesson_3/js/index_5.js
@@ -5,11 +5,9 @@ let answer;
 
 if (x < -2) {
     answer = 3 * x * x - 8;
-    alert(answer);
 } else if (x >= -2 && x <= 4) {
     answer = -9 * x * x - 12;
-    alert(answer); 
 } else {
     answer = 32 + x;
-    alert(answer);
 }
+alert(answer);
