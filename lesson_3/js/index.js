898d5e2fb46af8c837924ac738ab2132bf24574f
evgeniyskoblya <eugene.skoblya@gmail.com>
2019-02-23 13:41:13 +0200

    Lesson_3

--- lesson_3/js/index.js
+++ lesson_3/js/index.js
@@ -1,1 +1,37 @@
 'use strict'
+
+//LESSON 3
+
+// 1.
+
+let a = 1;
+let b = 2;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+alert("a = " + a + " b = " + b);
+
+//2.
+
+let num1 = prompt("Введите первое число: ", "");
+let num2 = prompt("Введите второе число: ", "");
+
+num1 == num2 ? alert("Первое и второе числа равны") :
+num1 > num2 ? alert(num1) : alert(num2);
+
+//3.
+
+let num = prompt("Введите номер квартиры: ", "");
+num <= 20 ?  alert("Это первый подъезд") :
+num <= 48 ?  alert("Это второй подъезд") :
+             alert("Это третий подъезд");
+
+//4.
+let now = 2019;
+let year = prompt("Введите год рождения: ", "");
+let yearsOld = 0;
+yearsOld = now - year;
+yearsOld >= 16 ?  alert("Вам " + yearsOld + " лет. " + "Добро пожаловать!") :
+                  alert("Вам " + yearsOld + " лет. " + "Вход воспрещен!")
