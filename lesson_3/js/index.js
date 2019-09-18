2d97d7893a7ee26d3994debba7c616ae91cb1064
Alexander Sanginov <alexgreenpol@gmail.com>
2019-09-18 10:46:14 +0300

    homework_lesson_3

--- lesson_3/js/index.js
+++ lesson_3/js/index.js
@@ -1,1 +1,57 @@
 'use strict'
+
+let a = 1; 
+let b = 2;
+if (a == 1) {
+	a = 2;
+}
+if (b == 2) {
+	b = 1
+}
+console.log('a =' + ' ' + a);
+console.log('b =' + ' ' + b);
+
+/*
+let numberOne = prompt('Введите первое число');
+let numberTwo = prompt('Введите второе число');
+let result;
+if (numberOne > numberTwo) {
+	result = numberOne;
+} else if (numberOne < numberTwo) {
+	result = numberTwo;
+} else {
+	result = 'Числа равны'
+}
+console.log(result);*/
+
+
+/*let roomNumber = prompt('Введите номер квартиры');
+if (roomNumber >= 1 && roomNumber <= 20) {
+	alert('Первый подъезд');
+} else if (roomNumber >= 21 && roomNumber <= 48) {
+	alert('Второй подъезд');
+} else if (roomNumber >= 49 && roomNumber <= 90) {
+	alert('Третий подъезд');
+} else {
+	alert('Такой квартиры в доме нет!');
+}*/
+
+
+/*let bornYear = prompt('Введите свой год рождения');
+if (2019 - Number(bornYear) >= 16) {
+	alert('Добро пожаловать!');
+} else {
+    alert('Вход запрещен!');
+}*/
+
+
+/*let x = prompt('Введите значение X');
+let resultFunction;
+if (x < -2) {
+	resultFunction = 3 * (+x * +x);
+} else if (2 <= x && x <= 4) {
+	resultFunction = -9 * (+x * +x) - 12;
+} else {
+	resultFunction = 32 + +x;
+}
+console.log('Резуальтат функции =' + ' ' + resultFunction);*/
