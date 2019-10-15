301ec35efd9fd88e22ed872966888aed4a0bc9f4
Alexander Sanginov <alexgreenpol@gmail.com>
2019-10-15 22:23:55 +0300

    lesson_8 ready home task

--- lesson_8/js/index.js
+++ lesson_8/js/index.js
@@ -1,1 +1,29 @@
 'use strict'
+/*Задача 1*/
+function randomInteger() {
+	let arr = [];
+	return function() {
+		let randomNumber = Math.ceil(Math.random() * 10);
+
+		if (arr.includes(randomNumber)) {
+			console.log('-');
+			return arr;
+		} else {
+			arr.push(randomNumber);
+		}
+		console.log(arr);
+	}
+}
+let randomIntegerGenerator = randomInteger();
+
+/*Задача 2*/
+
+function sequence(start = 0, step = 1) {
+	let counter = 0;
+	return function() {
+		counter++;
+		return counter == 1 ? start : start = start + step;
+	}
+}
+const generator = sequence(10, 3);
+const generator2 = sequence(7, 1);
