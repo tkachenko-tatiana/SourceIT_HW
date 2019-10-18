594f8aae92b6482638421b8bca646b227f90ea5e
Alexander Sanginov <alexgreenpol@gmail.com>
2019-10-18 10:37:10 +0300

    lesson_6 add changes

--- lesson_6/js/index.js
+++ lesson_6/js/index.js
@@ -2,22 +2,15 @@
 
 /*Матрица*/
 let arr = [];
+
 for (let i = 0; i < 4; i++) {
-	let newArr = [];
-	arr.push(newArr);
-	for (let j = 0; j < 4; j++) {
-		if (i == 3 && j == 0) {
-			newArr.push("1");
-		} else if (i == 2 && j == 1) {
-			newArr.push("1");
-		} else if (i == 1 && j == 2) {
-			newArr.push("1");
-		} else if (i == 0 && j == 3) {
-			newArr.push("1");
-		}
-		else {
-			newArr.push("0");
-		}
-	}
+	arr[i] = [];	
+    for (let j = 0; j < 4; j++) {
+        if (j == 3 - i) {
+            arr[i][j] = 1;
+        } else {
+            arr[i][j] = 0;
+        }
+    }
 }
 console.table(arr);
