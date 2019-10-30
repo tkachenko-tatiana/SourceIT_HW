217773374fa153c426cd4a1f7b741423d0a3bfb5
Alexander Sanginov <alexgreenpol@gmail.com>
2019-10-30 14:55:41 +0200

    lesson_12 ready home task

--- lesson_12/js/index.js
+++ lesson_12/js/index.js
@@ -1,1 +1,34 @@
 'use strict'
+
+/*Задача 1*/
+const group = {
+   users: ['student 1', 'student 2'],
+   sayHi: function(user) {
+     console.log(`Hi ${user}`);
+   },
+   sayHiToAll: function() {
+     this.users.forEach(user => this.sayHi(user))
+   }
+ }
+ group.sayHiToAll();
+
+ /*Задача 2*/
+function callWithTimer(func) {
+	return function() {
+		console.time('Execution time took');
+		let result = func();
+		console.timeEnd('Execution time took');
+		return result;
+	}
+}
+
+function testFunction() {
+ const arr = new Array(500).fill('test');
+ const newArr = arr.map((el, index) => `${el}_${index}`);
+ return newArr;
+}
+
+const testFunctionWrapper = callWithTimer(testFunction);
+const result = testFunctionWrapper();
+
+console.log('result: ', result)
