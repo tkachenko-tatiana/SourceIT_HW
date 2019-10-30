7d6257f745e8cd862cf551e7cf0ff98e53672c69
Victor <smartphonev@gmail.com>
2019-10-30 17:32:29 +0200

    lesson_12 homework done

--- lesson_12/js/index.js
+++ lesson_12/js/index.js
@@ -1,1 +1,39 @@
 'use strict'
+
+// #1
+
+const group = {
+    users: ['student 1', 'student 2'],
+    sayHi: function(user) {
+      console.log(`Hi ${user}`);
+    },
+    sayHiToAll: function() {
+      this.users.forEach(user => this.sayHi(user));
+    }
+  }
+   group.sayHiToAll();
+
+   // #2
+
+   function callWithTimer(func) {
+    // your magic!
+    return function (){
+        console.time('Duration');
+        let res = func();
+        console.timeEnd('Duration');
+        return res;
+    }
+    }
+   
+   
+   function testFunction() {
+    const arr = new Array(500).fill('test');
+   
+    const newArr = arr.map((el, index) => `${el}_${index}`);
+    return newArr;
+   }
+   
+   const testFunctionWrapper = callWithTimer(testFunction);
+   const result = testFunctionWrapper();
+   
+   console.log('result: ', result);
