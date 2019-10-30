4726e6d5ee9813fb7837e946a219e89dee2f2c20
Victor <smartphonev@gmail.com>
2019-10-30 17:46:23 +0200

    lesson_12 homework done

--- lesson_12/js/index.js
+++ lesson_12/js/index.js
@@ -4,36 +4,35 @@
 
 const group = {
     users: ['student 1', 'student 2'],
-    sayHi: function(user) {
-      console.log(`Hi ${user}`);
+    sayHi: function (user) {
+        console.log(`Hi ${user}`);
     },
-    sayHiToAll: function() {
-      this.users.forEach(user => this.sayHi(user));
+    sayHiToAll: function () {
+        this.users.forEach(user => this.sayHi(user));
     }
-  }
-   group.sayHiToAll();
+}
+group.sayHiToAll();
 
-   // #2
+// #2
 
-   function callWithTimer(func) {
-    // your magic!
-    return function (){
+function callWithTimer(func) {
+    return function () {
         console.time('Duration');
-        let res = func();
+        func();
         console.timeEnd('Duration');
-        return res;
+        return func();
     }
-    }
-   
-   
-   function testFunction() {
+}
+
+
+function testFunction() {
     const arr = new Array(500).fill('test');
-   
+
     const newArr = arr.map((el, index) => `${el}_${index}`);
     return newArr;
-   }
-   
-   const testFunctionWrapper = callWithTimer(testFunction);
-   const result = testFunctionWrapper();
-   
-   console.log('result: ', result);
+}
+
+const testFunctionWrapper = callWithTimer(testFunction);
+const result = testFunctionWrapper();
+
+console.log('result: ', result);
