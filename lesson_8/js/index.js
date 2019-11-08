6b9d1e641c1fde2c70294caaf1190398acd60d0b
Vladimir <vladnaum147@gmail.com>
2019-11-09 01:28:39 +0200

    lesson_8

--- lesson_8/js/index.js
+++ lesson_8/js/index.js
@@ -1,1 +1,25 @@
 'use strict'
+function makeRandom() {
+    let arr = [];
+    return function () {
+        let randomNumber = Math.floor(Math.random() * Math.floor(11));
+        if (arr.includes(randomNumber)) {
+            console.log('repeat!')
+            return arr
+        } else {
+            console.log(arr)
+            arr.push(randomNumber)
+        }
+    }
+}
+
+const randomArr = makeRandom();
+
+function sequence(start = 0, step = 1) {
+    return function () {
+        start = start + step
+        return start
+    }
+}
+const generator = sequence(10, 3);
+const generator2 = sequence(7, 1);
