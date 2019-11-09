cd15250555e198abee0598b1443f65b7145f07be
Vladimir <vladnaum147@gmail.com>
2019-11-09 02:10:26 +0200

    lesson_12

--- lesson_12/js/index.js
+++ lesson_12/js/index.js
@@ -1,1 +1,34 @@
 'use strict'
+
+//1
+const group = {
+    users: ['student 1', 'student 2'],
+    sayHi: function (user) {
+        console.log(`Hi ${user}`);
+    },
+    sayHiToAll: function () {
+        this.users.forEach((user) => {
+            this.sayHi(user)
+        })
+    }
+};
+group.sayHiToAll();
+
+
+//2
+function callWithTimer(func) {
+    return function () {
+        console.time(`${func.name} has been executed in`);
+        console.timeEnd(`${func.name} has been executed in`);
+        return func;
+    }
+}
+
+function testFunction() {
+    const arr = new Array(500).fill('test');
+    const newArr = arr.map((el, index) => `${el}_${index}`);
+    return newArr;
+}
+
+const testFunctionWrapper = callWithTimer(testFunction);
+const result = testFunctionWrapper();
