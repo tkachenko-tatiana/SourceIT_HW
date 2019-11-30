cc62788bdd0a59fc872ead3bceed156ff3cbcd34
Alexander Sanginov <alexgreenpol@gmail.com>
2019-11-30 09:45:44 +0200

    lesson_15 ready home task

--- lesson_15/js/index.js
+++ lesson_15/js/index.js
@@ -1,28 +1,27 @@
 'use strict'
 
 /*Задача №1*/
-
-let promise = new Promise(function(resolve, reject) {
-  function randomNumberHandler() {
-  setTimeout(() => {
-  	let randomNumber = Math.ceil(Math.random() * 10);
-  	if(randomNumber < 6) {
-  		resolve(console.log(`Успех! Сгенерировано число - ${parseInt(randomNumber, 10)}`));
-  	} else {
-  		reject(new Error(`Ошибка! Сгенерировано число - ${parseInt(randomNumber, 10)}.`));
-  	}
-  }, 2000);
+function randomNumberHandler() {
+  return new Promise(function(resolve, reject) {
+    setTimeout(() => {
+      let randomNumber = Math.ceil(Math.random() * 10);
+      if(randomNumber < 6) {
+        resolve(`Успех! Сгенерировано число - ${parseInt(randomNumber, 10)}`);
+      } else {
+        reject(new Error(`Ошибка! Сгенерировано число - ${parseInt(randomNumber, 10)}.`));
+      }
+    }, 2000);
+  })
 }
-randomNumberHandler();
-});
+randomNumberHandler().then(res => console.log(res))
 
 /*Задача №2*/
 
 function delay(ms) {
   return new Promise(function(resolve, reject) {
- 	setTimeout(() => {
- 		resolve();
- 	}, ms)
+  setTimeout(() => {
+    resolve();
+  }, ms)
   })
 }
 
@@ -31,13 +30,13 @@ delay(2000).then(() => alert("Hello!"));
 /*Задача №3*/
 
 function squarePow(num) {
-	let promise = new Promise(function(resolve, reject) {
-		setTimeout(() => {
-			resolve(console.log(parseInt(num *=num)));
-		}, 3000);
-	}).then(() => {
-		setTimeout(() => {console.log(num *=num)}, 3000)
-	})
+  return new Promise(function(resolve, reject) {
+    setTimeout(() => {
+      resolve(parseInt(num *=num));
+    }, 3000);
+  }).then(() => {
+    setTimeout(() => {num *=num}, 3000)
+  })
 }
 
 squarePow(5);
