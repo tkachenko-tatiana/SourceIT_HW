e3b63ab9ee2acee2a0aa9152f2e9ca0cf2af1315
DmitriiSigarev <dmitrysigarev@gmail.com>
2019-12-03 16:43:43 +0200

    lesson_7 ready home task

---
+++ lesson_7/8) price/js.js
@@ -0,0 +1,15 @@
+﻿let pro = [ { 
+	id: 1, 
+	name: "Морковь",
+	amount: 3,
+	price: 2.50 }
+	
+	]
+ 
+
+let x = [];
+ let getPrice = function(elem){
+ 	x.push(elem.price)
+ }
+	pro.forEach(getPrice);
+ console.log('Price: ' + x);
---
+++ lesson_7/9) категория/js.js
@@ -0,0 +1,25 @@
+﻿let list =[
+ { id: 1, name: "Морковь",
+ amount: 3,
+ price: 2.50,
+ category: "овощи"
+ },
+ { id: 1, name: "Яблоки",
+ amount: 10,
+ price: 21.70,
+ category: "фрукты"
+ },
+ { id: 1, name: "Футболки",
+ amount: 2,
+ price: 220,
+ category: "одежда"
+ }
+]
+
+let shirts = list.filter((shops)=>{
+	return shops.category === 'одежда'
+});
+
+console.log(shirts);
+	
+	
