5232db7610937f5b54c43d18d3bd96026610f43c
DmitriiSigarev <dmitrysigarev@gmail.com>
2019-12-03 16:47:22 +0200

    lesson_8 ready home task

---
+++ lesson_8/1)повтор числа/js.js
@@ -0,0 +1,23 @@
+﻿function func(){
+	const arr = [];
+	return function(){
+		const num = arr.push(Math.floor(Math.random()*10)+1);
+		
+		for(let i = 0; i<arr.length; i++){
+			if(arr[i] === num){
+				console.log(arr)
+
+			} else{
+				console.log("числа нет")
+			}
+				
+		}
+
+	}
+
+}
+
+const make = func();
+console.log(make())
+console.log(make())
+console.log(make())
---
+++ lesson_8/2)генератор/js.js
@@ -0,0 +1,13 @@
+﻿function sequence(start, step) {
+    start = start || 0;
+    step = step || 1;
+    start -= step;
+    return function() {
+        return start += step;
+    }
+}
+const generator = sequence(10,3);
+console.log(generator());
+console.log(generator());
+console.log(generator());
+console.log(generator());
