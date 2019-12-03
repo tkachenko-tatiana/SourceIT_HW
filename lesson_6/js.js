f6a1089ffc40348b128af33e8419bb749040ba28
DmitriiSigarev <dmitrysigarev@gmail.com>
2019-12-03 16:35:23 +0200

    lesson_6 ready home task

---
+++ lesson_6/js.js
@@ -0,0 +1,13 @@
+﻿let array = [];
+
+for (let i = 0; i < 6; i++) {
+	array[i] = [];	
+    for (let j = 0; j < 6; j++) {
+        if (j == 5 - i){
+        	array[i][j] = 1;
+        } else {
+            array[i][j] = 0;
+        }
+    }
+}
+console.table(array);
