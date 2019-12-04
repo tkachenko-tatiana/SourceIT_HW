96d56e51587e76d00a3b0a927f06c910e1bf36b1
Yuliia <51382944+Yuliia9226@users.noreply.github.com>
2019-12-04 14:19:43 +0200

    lesson_6 ready

--- lesson_6/js/index.js
+++ lesson_6/js/index.js
@@ -1,1 +1,17 @@
 'use strict'
+
+
+let arr = [];
+let n = 4;
+
+for (let i = 0; i < n; i++) {
+    arr[i] = [];
+    for (let j = 0; j < n; j++) {
+        if (3 - i == j) {
+            arr[i][j] = 1;
+        } else
+            arr[i][j] = 0;
+    }
+}
+
+console.table(arr);
