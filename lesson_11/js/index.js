32e3ebbafaed99b5be605b80576a08f303f2a75e
Yuliia <51382944+Yuliia9226@users.noreply.github.com>
2019-12-04 14:43:52 +0200

    lesson_11 ready

--- lesson_11/js/index.js
+++ lesson_11/js/index.js
@@ -1,1 +1,33 @@
 'use strict'
+
+const obj = {
+    a: 12,
+    b: 'I can do it',
+    c: {
+        d: {
+            e: 14,
+        },
+        f: 'test'
+    }
+}
+
+function deepClone(obj) {
+    if (typeof obj === 'object') {
+        return Object.keys(obj)
+            .map(function (item) {
+                return {
+                    [item]: deepClone(obj[item])
+                };
+            })
+            .reduce(function (item, clone) {
+                return Object.assign(item, clone)
+            });
+    } else {
+        return obj;
+    }
+}
+
+const clone = deepClone(obj);
+console.log(obj === clone)
+console.log(obj.c === clone.c)
+console.log(obj.c.d === clone.c.d)
