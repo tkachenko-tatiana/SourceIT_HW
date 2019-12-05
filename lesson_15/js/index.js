cbf7249b786d7c52fdb4013814869a45b6bb62ac
Yuliia <51382944+Yuliia9226@users.noreply.github.com>
2019-12-05 10:50:17 +0200

    lesson_15 ready

--- lesson_15/js/index.js
+++ lesson_15/js/index.js
@@ -1,1 +1,39 @@
 'use strict'
+
+//1
+function randomNumberHandler() {
+    let randomNumber = Math.ceil(Math.random() * 10);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (randomNumber <= 5) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, randomNumber * 1000)
+    });
+
+}
+
+//3
+
+function squarePow(num) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            resolve(parseInt(num *= num));
+        }, 3000);
+    }).then(() => {
+        setTimeout(() => {
+            num *= num
+        }, 3000)
+    })
+}
+
+//2
+function delay(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+}
