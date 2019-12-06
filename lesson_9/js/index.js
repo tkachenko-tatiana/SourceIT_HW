df441ba6ab425d86b9620ae71bcc01117c8c93c9
Yuliia9226 <kobecz2010@gmail.com>
2019-12-07 00:48:49 +0200

    lesson_9 ready

--- lesson_9/js/index.js
+++ lesson_9/js/index.js
@@ -1,1 +1,44 @@
 'use strict'
+
+//1
+
+function makeSpyOn(myFunc) {
+	let count = 0;
+	return function spyFunc() {
+		spyFunc.calls = count++;
+		myFunc();
+	}
+}
+
+const spy = makeSpyOn();
+spy();
+console.log(spy.calls)
+
+//4
+
+function calc(){
+	let num1;
+	let num2;
+
+	return{
+		input:() => {
+			num1 = +prompt('Первое число');
+			num2 = +prompt('Второе число');
+		},
+		sum:() => {
+			console.log(num1 + num2);
+		},
+		mul:() => {
+			console.log(num1 * num2);
+		},
+		sub:() => {
+			console.log(num1 - num2);
+		}
+
+	}
+
+}
+let calculator = calc();
+
+calculator.input();
+calculator.sum();
